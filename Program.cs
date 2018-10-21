7d3399d0e01d3701622388fc1260cab3b0075de0
Svyatix <Svyatix@users.noreply.github.com>
2018-10-21 22:57:10 +0500

    Task A
    1 первая задачка из лабы по гиту

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ramochka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int nLength = Length(N);
+            int tenPower = (int)Math.Pow(10, nLength - 1);
+
+            int result = 0;
+            while (N > 0)
+            {
+                int digit = N % 10;
+                N /= 10;
+
+                result += digit * tenPower;
+                tenPower /= 10;
+            }
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        static int Length(int number) {return (int)Math.Floor(Math.Log10(number)) + 1; }
+        
+    }
+    }
