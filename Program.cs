4826d5f6da8be1eebd04dbb1e3d963ecc836d618
Svyatoslav <43740183+Svyatix@users.noreply.github.com>
2018-10-29 00:16:41 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -33,7 +33,35 @@ namespace Ramochka
         //----------------------------- Задание В -------------------------//
         static void zadanieB
         {
-
+            int[] array = new int[13] { 1, 1, 1, 1, 1, 0, 2, 2, 2, 2, 3, 3, 3 };
+            int[] arrayCounter = new int[4] { 0, 0, 0, 0 };
+            int count = 1;
+            int max = 0;
+            int num = 0;
+            bool trig = false;
+            for (int i = 0; i < array.Length-1; i++)
+            {
+                 if (array[i] == array[i + 1])
+                 {
+                   count++;
+                   if (max < count) max = count;
+                 }
+                 else
+                   trig = true;
+                                    
+                 if (trig)
+                 {
+                    arrayCounter[num]=count;
+                    num++;
+                    trig = false;
+                    count = 1;
+                 }
+            }
+            int maxElem = arrayCounter[0];
+            for (int i = 0; i < arrayCounter.Length; i++)
+                if (arrayCounter[i] > maxElem) maxElem = arrayCounter[i];
+            Console.WriteLine("Максимальная длина=" + maxElem);
+            Console.ReadKey();
         }
     }
 }
